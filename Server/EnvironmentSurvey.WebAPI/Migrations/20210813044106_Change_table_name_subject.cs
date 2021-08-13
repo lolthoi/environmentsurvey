ce5869f19a629c0ae88882f1762c804fe983220a
Dương Hoàng <manchestervn1996@gmail.com>
2021-08-13 11:43:30 +0700

    migration db change table name Subjects

---
+++ Server/EnvironmentSurvey.WebAPI/Migrations/20210813044106_Change_table_name_subject.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace EnvironmentSurvey.WebAPI.Migrations
+{
+    public partial class Change_table_name_subject : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameTable(
+                name: "Subject",
+                newName: "Subjects");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Subjects",
+                table: "Subjects");
+
+            migrationBuilder.RenameTable(
+                name: "Subjects",
+                newName: "Subject");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Subject",
+                table: "Subject",
+                column: "Id");
+        }
+    }
+}
