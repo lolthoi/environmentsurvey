508fde91b1b5e69f4ddfaeee58dc168de5838dec
Cao Vuong Bach <58335662+cvb1994@users.noreply.github.com>
2021-08-16 18:39:48 +0700

    update admin user-result

---
+++ Frontend/Admin/js/viewJs/user-result.js
@@ -0,0 +1,38 @@
+var domen = "https://localhost:44304";
+
+var token = localStorage.getItem("token");
+var url_string = window.location + "";
+var url = new URL(url_string);
+var userId = url.searchParams.get("userId");
+
+$(document).ready(function(){
+    $.ajax({
+        type: "GET",
+        url: domen+"/api/"+userId+"/Result/",
+        headers: {
+            Authorization: "Bearer " + token,
+        },
+        contentType: "application/json; charset=utf-8",
+        datatype:"json",
+        async: true,
+        success: function(response) {
+            $("#box-title").val(response.User);
+            showList(response.Result);
+        },
+    });
+})
+
+function showList(list){
+    $("#listResultTbody").html("");
+    for(var i=0; i<list.length; i++){
+        $("#listResultTbody").append(
+            '<tr>'
+                +'<td>'+No+'</td>'
+                +'<td>'+list[i].Seminar+'</td>'
+                +'<td>'+list[i].Survey +'</td>'
+                +'<td>'+list[i].Point+'</td>'
+                +'<td>'+list[i].SubmitTime+'</td>'
+            +'</tr>'
+        )
+    }
+}
